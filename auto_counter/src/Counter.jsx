0207485b9d143f09d4f6872bba8a408dbe6a1638
catherine <catherinepgeorge02@gmail.com>
2026-02-09 22:39:04 +0530

    Modified code of auto_counter

--- auto_counter/src/Counter.jsx
+++ auto_counter/src/Counter.jsx
@@ -1,32 +1,58 @@
-import React, { useState } from 'react'
-import { Button } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
 
 function Counter() {
-    const [counter, setCounter] = useState(0) // setCounter -> update state variable 
+  const [counter, setCounter] = useState(0);
+  const [isRunning, setIsRunning] = useState(true); // true = counter running
 
-    // code for setting counter starts when page loads and keeps going until that button is clicked 
-    // when each time the button is clicked the button name "pause" and "resume" will change as toggle button
+  // useEffect to handle auto-counter
+  useEffect(() => {
+    let interval = null;
 
-    
-    function reset() {
-        setCounter(0);
+    if (isRunning) {
+      interval = setInterval(() => {
+        setCounter((prevCounter) => prevCounter + 1);
+      }, 1000);
+    } else if (!isRunning && interval !== null) {
+      clearInterval(interval);
     }
 
-    
-    return (
-        <div>
-            <div className='container border' style={{ marginTop: "200px" }}>
-                <div className='text-center'>
-                    <h1 className='text-primary'>Auto-Counter-Application</h1>
-                    <h1 className='fw-bolder'>{counter}</h1>
-                </div>
-                <div className='text-center m-3 p-4'>
-                    <Button variant="primary" className='m-2' >Increment</Button>
-                    <Button variant="secondary" className='m-2' onClick={reset}>Reset</Button>
-                </div>
-            </div>
+    // Cleanup on unmount or when isRunning changes
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  // Toggle pause/resume
+  const toggleCounter = () => {
+    setIsRunning(!isRunning);
+  };
+
+  // Reset counter
+  const reset = () => {
+    setCounter(0);
+  };
+
+  return (
+    <div>
+      <div className='container' style={{ marginTop: "200px" }}>
+        <div className='text-center'>
+          <h1 className='text-primary'>Auto-Counter-Application</h1>
+          <h1 className='fw-bolder'>{counter}</h1>
         </div>
-    )
+        <div className='text-center m-3 p-4'>
+          <Button 
+            variant={isRunning ? "danger" : "success"} 
+            className='m-2'
+            onClick={toggleCounter}
+          >
+            {isRunning ? "Pause" : "Resume"}
+          </Button>
+          <Button variant="secondary" className='m-2' onClick={reset}>
+            Reset
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Counter
+export default Counter;
