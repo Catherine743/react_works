86625e7bd7fc53c9bc2ff1df688d15f72c3633a3
catherine <catherinepgeorge02@gmail.com>
2026-02-09 22:14:05 +0530

    Modified code of resume-app

--- RB/resume-app/src/Pages/Pnf.jsx
+++ RB/resume-app/src/Pages/Pnf.jsx
@@ -4,11 +4,12 @@ import Button from '@mui/material/Button';
 
 function Pnf() {
   return (
-    <div className='text-center my-5'>
-      <h1>Page Not Found</h1>
-      <h4>Sorry, we couldn't find the page.</h4>
+    <div className='text-center'>
+      <img src="https://img.websitedesigningcompany.co.in/public/images/page_not_found/404.gif" height={'300px'} alt="" />
+      <h1 className='mb-5'>Page Not Found</h1>
+      <h4 className='mb-5'>Sorry, we couldn't find the page.</h4>
       <Link to={'/'}>
-        <Button variant="contained">
+        <Button variant="contained" className='mb-5'>
           Back to Home
         </Button>
       </Link>
