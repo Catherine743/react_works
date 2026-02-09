9f9b36ffcc847f49dcb2472f34cfa34e951eabea
catherine <catherinepgeorge02@gmail.com>
2026-02-09 13:15:01 +0530

    Modified code of resume-app

--- RB/resume-app/src/Components/Header.jsx
+++ RB/resume-app/src/Components/Header.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
-
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import { Link } from 'react-router-dom';
 function Header() {
+  const intro = 'A Resume Builder App is an essential tool for job seekers looking to create polished and effective resumes. By combining ease of use with professional design options, these apps empower users to present their qualifications confidently and increase their chances of landing job interviews.'
+
   return (
     <div>
-      Header
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <img width={'70px'} src="https://cdn-icons-gif.flaticon.com/16664/16664305.gif" alt="logo" />
+            </IconButton>
+
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to={'/'} style={{ textDecoration: "none", color: "white" }}>
+                Resume-Builder
+              </Link>
+            </Typography>
+
+            <Tooltip title={intro}>
+              <Button color="inherit">About</Button>
+            </Tooltip>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </div>
   )
 }
