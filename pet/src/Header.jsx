da0f9db63e3640b082dcd5a063d500a1e57ef3a7
catherine <catherinepgeorge02@gmail.com>
2026-02-24 20:28:11 +0530

    Modified code of resume-app

--- pet/src/Header.jsx
+++ pet/src/Header.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+
+  const location = useLocation();  // 👈 get current path
+
   return (
     <div className="bg-dark p-3 text-white d-flex justify-content-between">
       <h4>Pet Care Manager</h4>
+
       <div>
-        <Link to="/" className="text-white me-3">Home</Link>
-        <Link to="/add" className="text-white me-3">Add Pet</Link>
+        {/* ✅ Show Home link only if NOT on "/" */}
+        {location.pathname !== "/" && (
+          <Link to="/" className="text-white me-3">
+            Home
+          </Link>
+        )}
+
+        <Link to="/add" className="text-white me-3">
+          Add Pet
+        </Link>
       </div>
     </div>
   );
