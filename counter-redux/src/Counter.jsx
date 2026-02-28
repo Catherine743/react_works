2f7e1440d3f9430c6b0076a1a533e26f9bef3d22
catherine <catherinepgeorge02@gmail.com>
2026-02-28 12:40:20 +0530

    Modified code of counter-redux

--- counter-redux/src/Counter.jsx
+++ counter-redux/src/Counter.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from './Redux/slice/counterSlice'
 
 function Counter() {
+
+    const count = useSelector((state) => state.counterReducer.count)
+    const dispatch = useDispatch()
+
     return (
         <div>
             <div>
                 <h1 className='text-center fw-bolder mt-5 text-warning'>Counter-Application</h1>
                 <div className='container border border-dark mt-2 p-5 rounded w-50'>
-                    <h2 className='text-center mt-5'>0</h2>
+                    <h2 className='text-center mt-5'>{ count }</h2>
                     <div className='d-flex justify-content-around mt-5'>
-                        <Button variant="primary" >Increment</Button>
+                        <Button variant="primary" onClick={() => dispatch(increment())}>Increment</Button>
                         <Button variant="secondary" >Reset</Button>
                         <Button variant="success" >Decrement</Button>
                     </div>
