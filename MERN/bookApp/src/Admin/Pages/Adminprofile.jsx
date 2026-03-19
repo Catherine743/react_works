67ed028ef6ca54eb30c5fa9e034b1fb93a6ceb28
catherine <catherinepgeorge02@gmail.com>
2026-03-19 12:05:57 +0530

    Added auto-counter app in github

--- MERN/bookApp/src/Admin/Pages/Adminprofile.jsx
+++ MERN/bookApp/src/Admin/Pages/Adminprofile.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import Adminheader from '../Components/Adminheader'
+import Adminsidebar from '../Components/Adminsidebar'
 
 function Adminprofile() {
   return (
     <div>
-      
+      <Adminheader />
+      <div className='md:grid grid-cols-5 gap-2'>
+        <div className='col-span-1'>
+          <Adminsidebar />
+        </div>
+        <div className='col-span-4'>
+          <h1 className='text-3xl font-bold text-center my-5'>Settings</h1>
+          <div className='md:grid grid-cols-2 gap-5 mx-5 items-center'>
+            <div>
+              <p className='text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Nihil neque exercitationem possimus aut. Blanditiis illo magni quas, quae ipsa modi commodi quaerat eum sed
+                nesciunt iure provident, autem in temporibus?</p>
+              <p className='text-justify mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident blanditiis tempora perferendis quis voluptatibus earum necessitatibus.
+                Illum quia illo tempora sed vel, debitis possimus rem ullam corrupti reiciendis numquam id.</p>
+            </div>
+            <div className='rounded bg-blue-100 p-10 flex justify-center items-center flex-col mt-10 md:mt-0'>
+              <label htmlFor="adminPic" className='mb-3'>
+                <img src="https://img.freepik.com/premium-vector/man-character_665280-46970.jpg" width={'200px'} height={'200px'} alt="book" />
+                <input type="file" name='' id='adminPic' className='hidden' />
+              </label>
+              <div className='mb-3 w-full'>
+                <input type="text" className='p-2 bg-white border border-gray-200 text-black w-full rounded placeholder-gray-600' placeholder='Username'/>
+              </div>
+              <div className='mb-3 w-full'>
+                <input type="text" className='p-2 bg-white border border-gray-200 text-black w-full rounded placeholder-gray-600' placeholder='Password'/>
+              </div>
+              <div className='mb-3 w-full mt-20'>
+                <input type="text" className='p-2 bg-white border border-gray-200 text-black w-full rounded placeholder-gray-600' placeholder='Confirm Password'/>
+              </div>
+              <div className='mb-3 w-full flex justify-evenly'>
+                <button className='bg-orange-600 text-white'></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
